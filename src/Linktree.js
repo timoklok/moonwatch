f9b55bc3e4e0a0b35288f96ccd64a688c2ac503c
Timo Klok <mail@timoklok.nl>
2024-01-02 20:50:22 +0100

    add linktree page

---
+++ src/Linktree.js
@@ -0,0 +1,77 @@
+import "./Linktree.css";
+import React from "react";
+import styled from "styled-components";
+import { SiteTitle } from "./components/Text";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  max-width: 90vw;
+  margin: 0 auto;
+`;
+
+const Links = styled.div`
+  margin: 0vh 0;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  max-width: 600px;
+  width: 100%;
+`;
+
+const Link = styled.a`
+  display: block;
+  width: 100%;
+  margin: 8px 0;
+  padding: 16px;
+  color: white;
+  border: 1px solid white;
+  text-align: center;
+  text-decoration: none;
+  background: rgba(0, 0, 0, 0.5);
+  :hover {
+    background: white;
+    color: black;
+  }
+`;
+
+const LinkList = [
+  {
+    text: "Video for single Lūmen",
+    url: "https://www.youtube.com/watch?v=xnlJhhpedwg",
+  },
+  {
+    text: "Album release show tickets (De Nijverheid, Utrecht)",
+    url: "https://www.denijverheid.org/agenda/3079-dubbele-album-release-show-thistle-sifter-moonwatch",
+  },
+  {
+    text: "Website",
+    url: "/",
+  },
+];
+
+function Linktree() {
+  return (
+    <Container>
+      <SiteTitle>
+        Moon
+        <wbr />
+        watch
+      </SiteTitle>
+
+      <Links>
+        {LinkList.map((link, idx) => {
+          return (
+            <Link key={idx} href={link.url}>
+              {link.text}
+            </Link>
+          );
+        })}
+      </Links>
+    </Container>
+  );
+}
+
+export default Linktree;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import Linktree from "./Linktree";
 import reportWebVitals from "./reportWebVitals";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
@@ -9,6 +10,10 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+  },
+  {
+    path: "/linktree",
+    element: <Linktree />,
   },
 ]);
 
