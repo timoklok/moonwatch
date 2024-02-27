2d93d1b1c0eb9f52b290bc16c76b5cb341342237
Nils Breunese <nils@breun.nl>
2024-02-27 23:00:40 +0100

    Capitalize

--- src/views/Home.js
+++ src/views/Home.js
@@ -91,7 +91,7 @@ const Home = () => {
               />
             </a>
             <p>
-              Our first album <b>Myriad eyes \ Myriad Other</b> is out now!
+              Our first album <b>Myriad Eyes &bsol; Myriad Other</b> is out now!
               <br />
               <a href="#listen">Listen online</a>
             </p>
